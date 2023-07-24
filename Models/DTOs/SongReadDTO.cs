6058febbac367329a236b7489b9ed4453128f227
ConkyTheGreat <43240184+ChrisTheGreat1@users.noreply.github.com>
2023-07-24 17:36:14 -0400

    ordering, html, whitespace cleanup

--- Models/DTOs/SongReadDTO.cs
+++ Models/DTOs/SongReadDTO.cs
@@ -3,12 +3,12 @@
     public class SongReadDTO
     {
         public int Id { get; set; }
-        public string? AlbumArtLink { get; set; }
+        public required string Name { get; set; }
         public required string Artist { get; set; }
+        public string? AlbumArtLink { get; set; }
         public string? ArtistArtLink { get; set; }
         public int? DeezerId { get; set; }
         public string? Lyrics { get; set; }
-        public required string Name { get; set; }
         public DateTime QueryDate { get; set; }
         public int? SongDuration { get; set; }
     }
